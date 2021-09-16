ce34001ff80753422b56bbeef0692a1fd33028e1
jpromanonet <programming@jpromano.net>
2021-09-15 21:01:39 -0300

    Server

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
+import { Adapter } from 'socket.io-adapter';
 
 dotenv.config();
 
@@ -24,3 +25,13 @@ mongoose.connect(process.env.MONGODB_URL || 'ruta estatica local', {
 });
 
 // Definir los returns de nuestra API
+
+app.use('/api/uploads', uploadRouter);
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('api/orders', orderRouter);
+
+// Llamamos a APIs de terceros como Paypal o Stripe
+app.get('/api/config/paypal', (req, res) =>{
+    
+})
