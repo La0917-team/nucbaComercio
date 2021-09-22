ed04cbc787ab2d612abf9da4b2059e18b460d071
unknown <cristian.daniel.herrera@hotmail.com>
2021-09-22 13:43:19 +0100

    secondoCommit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Signin from './Components/Signin/Signin';
+import FormLogin from './Components/FormLogin/FormLogin'
 
 function App() {
-  return <Signin />;
+  return (
+    <>
+    <Signin/>
+    {/* <FormLogin/> */}
+    {/* Sigin y  FormLigin it's same component  */}
+    </>
+    
+  )
 }
 
 export default App;
